f75e3e3e54fa430e18760e70ccf640580861e677
Shamikh Hossain <ssh50@duke.edu>
2017-08-07 14:40:07 -0500

    added endif

--- inventory/views.py
+++ inventory/views.py
@@ -6,7 +6,7 @@ from .models import Item, Loaner
 # Create your views here.
 class IndexView(generic.ListView):
     template_name = 'inventory/index.html'
-    context_object_name = 'items'
+    context_object_name = 'items' # use to access list of items in index.html
 
     def get_queryset(self):
         return Item.objects.all()
