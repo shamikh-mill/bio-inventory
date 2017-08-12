91bc500a69595a48a6c86cb8463e4d2797ee688e
Shamikh Hossain <ssh50@duke.edu>
2017-08-12 18:27:06 -0500

    user profiles

--- inventory/python/services.py
+++ inventory/python/services.py
@@ -4,4 +4,4 @@ def display_name(netid):
 	url = 'https://streamer.oit.duke.edu/ldap/people/netid/'+ netid +'?access_token=108332719d22af28cbf2a1c0cdd99371'
 	with urllib.request.urlopen(url) as url:
 	    data = json.loads(url.read().decode())
-	    return (data[0]['display_name'])
+	    return ([data[0]['display_name'], data[0]['emails'][0], [0]['primary_affiliation']])
