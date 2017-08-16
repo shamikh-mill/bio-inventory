1400ed33c1863ab68029f9e58f145b400f350c4f
Shamikh Hossain <ssh50@duke.edu>
2017-08-15 19:26:31 -0500

    links

--- inventory/views.py
+++ inventory/views.py
@@ -40,7 +40,6 @@ class ItemCreate(CreateView):
 class ItemUpdate(UpdateView):
     model = Item
     fields = ['make_and_model', 'label', 'notes', 'checked_out']
-    template_name_suffix = '_update_form'
 
 
 class ItemDelete(DeleteView):
