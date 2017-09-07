0c13da58d948e179ab5d33a50695551c09fdd4dc
Shamikh Hossain <ssh50@duke.edu>
2017-09-07 15:12:49 -0400

    URL issue

--- inventory/urls.py
+++ inventory/urls.py
@@ -5,7 +5,7 @@ app_name = 'inventory'
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name = 'index'),
-    url(r'in-stock/$', views.InStock.as_view(), name = 'in-stock'),
+    url(r'stock/$', views.InStock.as_view(), name = 'in-stock'),
     url(r'checked-out/$', views.OutofStock.as_view(), name = 'checked-out'),
     url(r'item/add/$', views.ItemCreate.as_view(), name = "item-add"),
     url(r'loaner/add/$', views.LoanerCreate.as_view(), name = "loaner-add"),
